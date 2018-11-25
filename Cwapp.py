aa4a570e4b92efc09402e7dab077d9f53a8006b1
Connor Ness <Connorrness@Gmail,com>
2018-11-25 17:13:20 +0000

    update pull

--- Cwapp.py
+++ Cwapp.py
@@ -18,19 +18,27 @@ def close_db_connection(exception):
         db.close()
 
 class MysticCode:
-    def __init__(self, name, desc):
-        self.name = name
+    def __init__(self, mname, desc):
+        self.mname = mname
         self.desc = desc
 
 class Servants:
-    def __init__(self, name, NP, Rarity, S1, S2, S3, sclass, desc):
+    def __init__(self, name, NP, Rarity, scost, S1, S2, S3, sclass, desc):
         self.name = name
         self.NP = NP
         self.Rarity = Rarity
+        self.scost = scost
         self.S1 = S1
         self.S2 = S2
         self.S3 = S3
         self.sclass = sclass
+        self.desc = desc
+
+class CraftEssence:
+    def __init__(self, cname, ccost, crarity, desc):
+        self.cname = cname
+        self.ccost = ccost
+        self.crarity = crarity
         self.desc = desc
 
 @app.route('/')
@@ -42,17 +50,17 @@ def index ():
 def MysticCodes():
     MysticCodenames = []
     db = get_db()
-    sql = "SELECT name FROM MysticCodes"
+    sql = "SELECT mname FROM MysticCodes"
     for row in db.cursor().execute(sql):
         MysticCodenames.append(row[0])
 
-    return render_template('MysticCodeList.html', names=MysticCodenames)
+    return render_template('MysticCodeList.html', mnames=MysticCodenames)
 
 @app.route('/MysticCodes/<MysticCodename>')
 def MysticCodename(MysticCodename):
     db = get_db()
-    sql = "SELECT * FROM MysticCodes WHERE name=:name"
-    result = db.cursor().execute(sql, {"name":MysticCodename}).fetchone()
+    sql = "SELECT * FROM MysticCodes WHERE mname=:mname"
+    result = db.cursor().execute(sql, {"mname":MysticCodename}).fetchone()
     thisMysticCode = MysticCode(result[0], result[1])
 
     return render_template('MysticCodeDesc.html', MysticCode=thisMysticCode)
@@ -68,15 +76,48 @@ def servants():
 
     return render_template('servantnames.html', names=servantnames)
 
-
 @app.route('/servants/<servantname>')
 def servant_name(servantname):
     db = get_db()
     sql = "SELECT * FROM servants WHERE name=:name"
     result = db.cursor().execute(sql, {"name":servantname}).fetchone()
-    thisServant = Servants(result[0], result[1], result[2], result[3], result[4], result[5], result[6], result[7])
+    thisServant = Servants(result[0], result[1], result[2], result[3], result[4], result[5], result[6], result[7], result[8])
 
     return render_template('servantDesc.html', Servant=thisServant)
+
+
+@app.route('/CraftEssences')
+def CraftEssences():
+    CraftEssencenames = []
+    db = get_db()
+    sql = "SELECT cname FROM CraftEssences"
+    for row in db.cursor().execute(sql):
+        CraftEssencenames.append(row[0])
+
+    return render_template('CraftList.html', cnames=CraftEssencenames)
+
+@app.route('/CraftEssences/<CraftEssencename>')
+def CraftEssencename(CraftEssencename):
+    db = get_db()
+    sql = "SELECT * FROM CraftEssences WHERE cname=:cname"
+    result = db.cursor().execute(sql, {"cname":CraftEssencename}).fetchone()
+    thisCraftEssence = CraftEssence(result[0], result[1], result[2], result[3])
+
+    return render_template('CraftDesc.html', CraftEssence=thisCraftEssence)
+
+@app.route('/party')
+def party():
+    partyservants = []
+    partyCE = []
+    db = get_db()
+    sql = "SELECT name FROM servants"
+    csql = "SELECT cname FROM CraftEssences"
+    for row in db.cursor().execute(sql):
+        partyservants.append(row[0])
+    for row in db.cursor().execute(csql):
+        partyCE.append(row[0])
+
+    return render_template('party.html', names=partyservants, cnames=partyCE)
 
 
 @app.route('/send', methods=['GET', 'POST'])
@@ -85,19 +126,39 @@ def send():
         servantName = request.form['servantname']
         servantNP = request.form['NP']
         servantRarity = request.form['Rarity']
+        servantCost = request.form['scost']
         servantS1 = request.form['S1']
         servantS2 = request.form['S2']
         servantS3 = request.form['S3']
-        servantSClass = request.form['ServantClass']
-        servantDesc = request.form['description']
-        print(servantName, servantNP, servantRarity, servantS1, servantS2, servantS3, servantClass, servantDesc)
+        servantSClass = request.form['sclass']
+        servantDesc = request.form['desc']
+        print(servantName, servantNP, servantRarity, servantCost, servantS1, servantS2, servantS3, servantSClass, servantDesc)
 
 
         db = get_db()
-        sql = "INSERT INTO servants VALUES(?,?,?,?,?,?)"
-        db.cursor().execute(sql,(servantName, servantS1, servantS2, servantS3, servantClass, servantDesc))
+        sql = "INSERT INTO servants VALUES(?,?,?,?,?,?,?,?,?)"
+        db.cursor().execute(sql,(servantName, servantNP, servantRarity, servantCost, servantS1, servantS2, servantS3, servantSClass, servantDesc))
         db.commit()
 
         return redirect(url_for('index'))
 
     return render_template('input.html')
+
+@app.route('/sendCE', methods=['GET', 'POST'])
+def sendCE():
+    if request.method == 'POST':
+        craftName = request.form['craftname']
+        craftRarity = request.form['crarity']
+        craftCost = request.form['ccost']
+        craftDesc = request.form['cdesc']
+        print(craftName, craftRarity, craftCost, craftCost, craftDesc)
+
+
+        db = get_db()
+        sql = "INSERT INTO CraftEssences VALUES(?,?,?,?)"
+        db.cursor().execute(sql,(craftName, craftRarity, craftCost, craftDesc))
+        db.commit()
+
+        return redirect(url_for('index'))
+
+    return render_template('inputCE.html')
